c861cf925d52b0d7f9e2e96b9d717996860ecf9b
Thanathorn Nilkhao <thanathorn@outlook.com>
2020-09-15 10:07:04 +0700

    Move file
    ~Move Shader file path to Source folder

---
+++ SrandEngine/SrandEngine/Source/Shader.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+
+/* OpenGL */
+#include <GLFW/glfw3.h>
+#include <GL/glew.h>
+
+
+static unsigned int CompileShader(unsigned int type, const std::string& source);
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+void InitializeShader();
+void DeleteShader();
