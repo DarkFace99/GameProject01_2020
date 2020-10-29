cda83846346d2b77c52be4c58897efb73d12e23e
Akira <akira.rds01@gmail.com>
2020-10-29 23:03:48 +0700

    Define Engine f()
    + Engine.cpp

--- SrandEngine/SrandEngine/Source/Engine.cpp
+++ SrandEngine/SrandEngine/Source/Engine.cpp
@@ -1,1 +1,75 @@
 #include "Engine.h"
+
+Engine* Engine::s_instance = nullptr;
+
+Engine::Engine() {
+	window = nullptr;
+	running = false;
+}
+
+void Engine::Init() {
+    
+    /* Initialize the library */
+    std::cout << "Initializing GLFW..." << std::endl;
+    if (!glfwInit())
+    {
+        std::cout << "Error! Cannot initializing GLFW" << std::endl;
+    }
+
+    /* Create a windowed mode window and its OpenGL context */
+    std::cout << "Initializing Window..." << std::endl;
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGTH, WINDOW_NAME, (FULLSCREEN) ? glfwGetPrimaryMonitor() : NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        std::cout << "Error! Cannot create window" << std::endl;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /*Vsync on = 1, off = 0*/
+    glfwSwapInterval(1);
+
+    /*Initializing GLEW*/
+    std::cout << "Initializing GLEW..." << std::endl;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Error! Cannot initializing GLEW" << std::endl;
+    }
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    std::cout << "--------------------------------------------------------------------------------" << std::endl;
+    std::cout << "                            |--System Initialized--|                            " << std::endl;
+    std::cout << "--------------------------------------------------------------------------------" << std::endl;
+    std::cout << "                          Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "--------------------------------------------------------------------------------" << std::endl;
+
+    running = true;
+}
+
+void Engine::Draw(){
+
+    glClearColor(0.3f, 0.3f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+}
+
+void Engine::Update() {
+
+}
+
+void Engine::FixedUpdate() {
+
+}
+
+void Engine::Clean() {
+    AssetManager::get().Clean();
+
+    std::cout << "Closing window..." << std::endl << "System Shutdown" << std::endl;
+    glfwTerminate();
+}
+
+void Engine::Quit() {
+    running = false;
+}
--- SrandEngine/SrandEngine/Source/Engine.h
+++ SrandEngine/SrandEngine/Source/Engine.h
@@ -1,22 +1,33 @@
 #pragma once
 
 // Include GLFW
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
+/* Custom Header */
+#include "AssetManager.h"
 
 /* Initialize Window */
 #define SCREEN_WIDTH    1280
-#define SCREEN_HEIGTH  720
+#define SCREEN_HEIGTH   720
+#define WINDOW_NAME		"BENNY - Everyone is Happy"
+#define FULLSCREEN		false
 
 class Engine {
 private:
 	GLFWwindow* window;
-	bool isRunning;
 	static Engine* s_instance;
-
+	bool running;
+	
 public:
-	void Quit();
+	Engine();
+	~Engine() = default;
+
 	void Init();
 	void Clean();
+	void Quit();
 
 	void Draw();
 	void Update();
@@ -27,5 +38,9 @@ public:
 			s_instance = new Engine();
 		}
 		return *s_instance;
+	}
+
+	inline bool IsRunning() {
+		return running;
 	}
 };
