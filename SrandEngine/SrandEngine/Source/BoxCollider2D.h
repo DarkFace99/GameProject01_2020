6ccba594adb80cef5ca2e36cabc163beb6094d94
Akira <akira.rds01@gmail.com>
2020-12-01 14:33:34 +0700

    Collision Constructor

--- SrandEngine/SrandEngine/Source/BoxCollider2D.h
+++ SrandEngine/SrandEngine/Source/BoxCollider2D.h
@@ -22,17 +22,19 @@ private:
 	Camera* camera;
 
 public:
-	BoxCollider2D(std::string tag, float width, float height, std::string meshID, Camera* camera, bool overlap = false, bool movable = false)
-		: collisionTag(tag), width(width), height(height), meshID(meshID), camera(camera), allowOverlap(overlap), movable(movable){}
+	BoxCollider2D(std::string tag, float width, float height, bool overlap = false, bool movable = false, std::string meshID = "", Camera* camera = nullptr)
+		: collisionTag(tag), width(width), height(height), allowOverlap(overlap), movable(movable), meshID(meshID), camera(camera) {}
 
-	BoxCollider2D(float width, float height, std::string meshID, Camera* camera, bool overlap = false, bool movable = false)
-		: width(width), height(height), meshID(meshID), camera(camera), allowOverlap(overlap), movable(movable){}
+	BoxCollider2D(float width, float height, bool overlap = false, bool movable = false, std::string meshID = "", Camera* camera = nullptr)
+		: width(width), height(height), allowOverlap(overlap), movable(movable), meshID(meshID), camera(camera) {}
 
 	std::string GetTag() { return collisionTag; }
 
 	bool Init() override final {
 		transform = &gameObject->GetComponent<Transform>();
-		mesh = AssetManager::get().GetMesh(meshID);
+		if (meshID != "") {
+			mesh = AssetManager::get().GetMesh(meshID);
+		}
 		return true;
 	}
 
@@ -40,25 +42,27 @@ public:
 	}
 
 	void Draw() override final {
-		glUseProgram(Shader::get()->shader);
+		if (mesh != nullptr && camera != nullptr) {
+			glUseProgram(Shader::get()->shader);
 
-		//Set the render mode between Texture and Solid Color						// glGetUniformLocation called every frame is not efficient
-		glUniform1i(glGetUniformLocation(Shader::get()->shader, "mode"), 0);
-		glUniform1f(glGetUniformLocation(Shader::get()->shader, "alpha"), 1.0f);
+			//Set the render mode between Texture and Solid Color						// glGetUniformLocation called every frame is not efficient
+			glUniform1i(glGetUniformLocation(Shader::get()->shader, "mode"), 0);
+			glUniform1f(glGetUniformLocation(Shader::get()->shader, "alpha"), 1.0f);
 
-		/*Set transform of the objects relative to camera*/
-		// If there's no changing in scale and rotate on run time, move the modelMat to Init() and only calculate position here.
-		glm::mat4 modelMat = glm::mat4(1.0f);
-		modelMat = glm::translate(modelMat, glm::vec3(transform->position.x, transform->position.y, 0));
-		modelMat = glm::scale(modelMat, glm::vec3(transform->scale.x, transform->scale.y, 1));
-		modelMat = glm::rotate(modelMat, transform->rotationAngle * 3.14f / 180.0f, glm::vec3(0, 0, 1) /* rotate z-axis */);
-		glm::mat4 MVP = camera->GetProjectionMat() * camera->GetViewMatrix() * modelMat;
-		glUniformMatrix4fv(glGetUniformLocation(Shader::get()->shader, "MVP"), 1, GL_FALSE, &MVP[0][0]);
+			/*Set transform of the objects relative to camera*/
+			// If there's no changing in scale and rotate on run time, move the modelMat to Init() and only calculate position here.
+			glm::mat4 modelMat = glm::mat4(1.0f);
+			modelMat = glm::translate(modelMat, glm::vec3(transform->position.x, transform->position.y, 0));
+			modelMat = glm::scale(modelMat, glm::vec3(transform->scale.x, transform->scale.y, 1));
+			modelMat = glm::rotate(modelMat, transform->rotationAngle * 3.14f / 180.0f, glm::vec3(0, 0, 1) /* rotate z-axis */);
+			glm::mat4 MVP = camera->GetProjectionMat() * camera->GetViewMatrix() * modelMat;
+			glUniformMatrix4fv(glGetUniformLocation(Shader::get()->shader, "MVP"), 1, GL_FALSE, &MVP[0][0]);
 
-		// Draw using mesh
-		glBindVertexArray(mesh->vao);
-		glDrawArrays(GL_LINE_LOOP, 0, mesh->vertex.size());
+			// Draw using mesh
+			glBindVertexArray(mesh->vao);
+			glDrawArrays(GL_LINE_LOOP, 0, mesh->vertex.size());
 
-		glBindVertexArray(0);
+			glBindVertexArray(0);
+		}
 	}
 };
