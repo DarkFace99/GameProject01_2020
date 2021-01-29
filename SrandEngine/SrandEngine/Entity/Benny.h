46572746702f3c39768a55e1d00ac65d52bb3f49
Akira <akira.rds01@gmail.com>
2021-01-30 00:57:03 +0700

    Create function that Check benny distace with other CCs

--- SrandEngine/SrandEngine/Entity/Benny.h
+++ SrandEngine/SrandEngine/Entity/Benny.h
@@ -1,9 +1,29 @@
 #pragma once
-
+#include <vector>
 #include "CC.h"
 
 class Benny : public CC {
 public:
 	Benny() { SetTag(ccTag::BENNY); }
 	~Benny() = default;
+
+	void AbilityCheck() {
+		for (int i = 0; i < SceneManager::get().objManager.size(); i++) {
+			if (SceneManager::get().objManager[i]->HasComponent<CC>()) {
+				Vector2D_float deltaVect = GetTransform().position - SceneManager::get().objManager[i]->GetComponent<Transform>().position;
+				float magnitude = sqrt(pow(deltaVect.x, 2) + pow(deltaVect.y, 2));
+				if (magnitude < radius) {
+					connectList.push_back(SceneManager::get().objManager[i]);
+				}
+			}
+		}
+	}
+
+	bool init() {
+		SetTransform();
+	}
+
+private:
+	float radius = 20.0f;	// place holder 
+	std::vector<GameObject*>connectList;
 };
--- SrandEngine/SrandEngine/Entity/CC.h
+++ SrandEngine/SrandEngine/Entity/CC.h
@@ -20,8 +20,15 @@ public:						// Not sure if this the best way to implement it.
 
 	void SetTag(ccTag cctag) { CC:tag = cctag; }	// Not use directly, it will be called in the
 													// constructor of each derived CC class.
-	ccTag GetTag(){ return tag; }	// to check CC type.
+	ccTag GetTag() { return tag; }	// to check CC type.
+
+	void SetTransform() { transform = &gameObject->GetComponent<Transform>(); }
+	Transform GetTransform(){ return *transform; }
 
 private:
+	Transform* transform = nullptr;
 	ccTag tag = ccTag::DEFAULT;
+
+	bool inControl = false;
+	Transform* targetTransform = nullptr;
 };
